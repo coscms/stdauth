12dbfa6748eca55f6f4acfcc7c02be5e6d12d006
Hank Shen <swhbox@foxmail.com>
2026-05-22 11:31:27 +0800

    update

--- sign.go
+++ sign.go
@@ -6,6 +6,18 @@ import (
 	"net/url"
 )
 
+// MakeSign 生成签名
+// 规则说明：数据按照以键的字母顺序排列后进行url编码，然后拼接上`&secret=密钥`
+// 等价的PHP代码：
+// ```php
+// <?php
+//
+//	function makeSign(array $data, string $secret): string{
+//		ksort($data);
+//		return hash('sha256',http_build_query($data).'&secret='.$secret);
+//	}
+//
+// ```
 func MakeSign(data url.Values, secret string) string {
 	h := sha256.Sum256([]byte(data.Encode() + `&secret=` + secret))
 	return fmt.Sprintf("%x", h)
